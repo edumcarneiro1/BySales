91ffbefdc2cc036b621e0137e94ff51a85310f88
Eduardo Carneiro <eduardo.carneiro@jumpseller.com>
2017-03-10 13:53:23 +0000

    page section on front-end finished

---
+++ public/javascripts/components/js/page.js
@@ -0,0 +1,74 @@
+"use strict";
+
+var Page = React.createClass({
+  displayName: "Page",
+
+  render: function render() {
+    return React.createElement(
+      "section",
+      { className: "portfolio-details page-section" },
+      React.createElement(
+        "div",
+        { className: "container" },
+        React.createElement(
+          "div",
+          { className: "row pt-10" },
+          React.createElement(
+            "div",
+            { className: "col-md-8 col-md-offset-2" },
+            React.createElement(
+              "div",
+              { className: "project-desc  body-section" },
+              React.createElement(
+                "h3",
+                null,
+                this.props.page.title
+              ),
+              this.props.page.body
+            ),
+            React.createElement(
+              "div",
+              { className: "post-share" },
+              React.createElement(
+                "ul",
+                null,
+                React.createElement(
+                  "li",
+                  null,
+                  "Share: "
+                ),
+                React.createElement(
+                  "li",
+                  null,
+                  React.createElement(
+                    "a",
+                    { href: "#" },
+                    React.createElement("i", { className: "fa fa-facebook" })
+                  )
+                ),
+                React.createElement(
+                  "li",
+                  null,
+                  React.createElement(
+                    "a",
+                    { href: "#" },
+                    React.createElement("i", { className: "fa fa-twitter" })
+                  )
+                ),
+                React.createElement(
+                  "li",
+                  null,
+                  React.createElement(
+                    "a",
+                    { href: "#" },
+                    React.createElement("i", { className: "fa fa-instagram" })
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    );
+  }
+});
---
+++ public/javascripts/components/src/page.jsx
@@ -0,0 +1,26 @@
+var Page = React.createClass({
+  render: function() {
+    return(
+      <section className="portfolio-details page-section">
+        <div className="container">
+          <div className="row pt-10">
+            <div className="col-md-8 col-md-offset-2">
+              <div className="project-desc  body-section">
+                <h3>{this.props.page.title}</h3>
+                {this.props.page.body}
+              </div>
+              <div className="post-share">
+                <ul>
+                  <li>Share: </li>
+                  <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                  <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                  <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+});
